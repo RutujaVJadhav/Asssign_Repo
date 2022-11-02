be3ea3bb932b3a43aed7c5e64187ff81e65cb80b
Jadhav <Rutuja.Jadhav@emtecinc.com>
2022-11-02 17:22:45 +0530

    React Assignment 1 2

---
+++ ReactAssignments/assignment2/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+//import './App.css';
+import Login from './components/Login'
+import Register from './components/Register';
+import Counter from './components/Counter';
+import Rating from './components/Rating';
+function App() {
+  return (
+    <div className="App">
+  {/*<Login/>
+  <Register/>*/}
+  <Counter/>
+  <Rating/>
+    </div>
+  );
+}
+
+export default App;
